370a53263e55ac5da35620513ff2373c5f15e2a2
Borze Andrei <borzeandrew@gmail.com>
2022-12-04 17:54:13 +0200

    Add project files.

---
+++ lab2ex2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab2ex2
+{
+    /* Ex 2: Scrieti un program care va afisa factorialul unui numar n, n fiind citit de la tastatura. */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Acest program va afisa factorialul unui numar n, n fiind citit de la tastatura. \n");
+
+            long nr;
+            Console.WriteLine("Introduceti un numar:");
+            bool success = long.TryParse(Console.ReadLine(), out nr);
+
+            if (success)
+            {
+                Console.WriteLine("Factorial: " + getFactorial(nr));
+            }
+            else
+            {
+                Console.WriteLine(" Atentie! Ai introdus si litere!");
+            }
+        }
+
+        private static long getFactorial(long nr)
+        {
+            var fac = 1;
+
+           for(var i = 1; i <= nr; i++)
+           {
+                fac = fac * i;
+           }
+
+            return fac;
+        }
+    }
+}
